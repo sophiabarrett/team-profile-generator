5db7532779e0ad1f78eb5e6c51b0126e2cf3ea60
Sophia Barrett <sophia@yoursummit.media>
2021-11-11 18:01:57 -0700

    write tests for manager constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,15 @@
 const Employee = require('../lib/Employee');
 
+const obj = {
+    name: 'Jane Doe',
+    id: '0123456789',
+    email: 'janedoe@company.com'
+}
+
 describe('Employee', () => {
     describe('constructor', () => {
         it('should create employee object', () => {
-            const employee = new Employee('Jane Doe', '0123456789', 'janedoe@company.com');
+            const employee = new Employee(obj);
 
             expect(employee.name).toBe('Jane Doe');
             expect(employee.id).toBe('0123456789');
@@ -13,7 +19,7 @@ describe('Employee', () => {
 
     describe('getName', () => {
         it("should return employee's name", () => {
-            const employee = new Employee('Jane Doe', '0123456789', 'janedoe@company.com');
+            const employee = new Employee(obj);
 
             expect(employee.getName()).toBe('Jane Doe');
         });
@@ -21,7 +27,7 @@ describe('Employee', () => {
 
     describe('getId', () => {
         it("should return employee's id", () => {
-            const employee = new Employee('Jane Doe', '0123456789', 'janedoe@company.com');
+            const employee = new Employee(obj);
 
             expect(employee.getId()).toBe('0123456789');
         });
@@ -29,7 +35,7 @@ describe('Employee', () => {
 
     describe('getEmail', () => {
         it("should return employee's email", () => {
-            const employee = new Employee('Jane Doe', '0123456789', 'janedoe@company.com');
+            const employee = new Employee(obj);
 
             expect(employee.getEmail()).toBe('janedoe@company.com');
         });
@@ -37,7 +43,7 @@ describe('Employee', () => {
 
     describe('getRole', () => {
         it('should return "Employee"', () => {
-            const employee = new Employee('Jane Doe', '0123456789', 'janedoe@company.com');
+            const employee = new Employee(obj);
 
             expect(employee.getRole()).toBe('Employee');
         });
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,53 @@
+const Manager = require('../lib/Manager');
+
+const obj = {
+    name: 'Jane Doe',
+    id: '0123456789',
+    email: 'janedoe@company.com',
+    officeNum: '480-440-8888'
+}
+
+describe('Manager', () => {
+    describe('constructor', () => {
+        it('should create Manager object', () => {
+            const manager = new Manager(obj);
+
+            expect(manager.name).toBe('Jane Doe');
+            expect(manager.id).toBe('0123456789');
+            expect(manager.email).toBe('janedoe@company.com');
+            expect(manager.officeNum).toBe('480-440-8888');
+        });
+    });
+
+    describe('getName', () => {
+        it("should return manager's name", () => {
+            const manager = new Manager(obj);
+
+            expect(manager.getName()).toBe('Jane Doe');
+        });
+    });
+
+    describe('getId', () => {
+        it("should return manager's id", () => {
+            const manager = new Manager(obj);
+
+            expect(manager.getId()).toBe('0123456789');
+        });
+    });
+
+    describe('getEmail', () => {
+        it("should return manager's email", () => {
+            const manager = new Manager(obj);
+
+            expect(manager.getEmail()).toBe('janedoe@company.com');
+        });
+    });
+
+    describe('getRole', () => {
+        it('should return "Manager"', () => {
+            const manager = new Manager(obj);
+
+            expect(manager.getRole()).toBe('Manager');
+        });
+    });
+});
