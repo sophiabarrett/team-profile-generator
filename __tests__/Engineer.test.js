bc4fdffbf41163434bea617ad156f9b92b48c652
Sophia Barrett <sophia@yoursummit.media>
2021-11-11 18:16:28 -0700

    write tests for engineer constructor

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,61 @@
+const Engineer = require('../lib/Engineer');
+
+const obj = {
+    name: 'Jane Doe',
+    id: '0123456789',
+    email: 'janedoe@company.com',
+    github: 'janedoe'
+}
+
+describe('Engineer', () => {
+    describe('constructor', () => {
+        it('should create Engineer object', () => {
+            const engineer = new Engineer(obj);
+
+            expect(engineer.name).toBe('Jane Doe');
+            expect(engineer.id).toBe('0123456789');
+            expect(engineer.email).toBe('janedoe@company.com');
+            expect(engineer.github).toBe('janedoe');
+        });
+    });
+
+    describe('getName', () => {
+        it("should return engineer's name", () => {
+            const engineer = new Engineer(obj);
+
+            expect(engineer.getName()).toBe('Jane Doe');
+        });
+    });
+
+    describe('getId', () => {
+        it("should return engineer's id", () => {
+            const engineer = new Engineer(obj);
+
+            expect(engineer.getId()).toBe('0123456789');
+        });
+    });
+
+    describe('getEmail', () => {
+        it("should return engineer's email", () => {
+            const engineer = new Engineer(obj);
+
+            expect(engineer.getEmail()).toBe('janedoe@company.com');
+        });
+    });
+
+    describe('getRole', () => {
+        it('should return "Engineer"', () => {
+            const engineer = new engineer(obj);
+
+            expect(engineer.getRole()).toBe('Engineer');
+        });
+    });
+
+    describe('getGithub', () => {
+        it("should return engineer's github username", () => {
+            const engineer = new engineer(obj);
+
+            expect(engineer.getGithub()).toBe('janedoe');
+        });
+    });
+});
